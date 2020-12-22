33d6e34cb099865f81638ab04a652c337df46eda
Long_kkl Gaming <longkklr@gmail.com>
2020-12-22 15:50:38 +0000

    added setup function in economy

--- cogs/economy.py
+++ cogs/economy.py
@@ -2,3 +2,11 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import BucketType
 
+class economy(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        
+
+def setup(client):
+	client.add_cog(economy(client)) 
