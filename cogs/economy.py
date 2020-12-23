a64f2124a73324f3dc7b40a17c97db9835d5d831
InsrtRandomUserHere <72641014+InsrtRandomUserHere@users.noreply.github.com>
2020-12-23 23:16:45 +0800

    added inventory, shop, sell, buy commands

--- cogs/economy.py
+++ cogs/economy.py
@@ -6,6 +6,12 @@ from random import randint
 import random
 
 embedColor = discord.Colour.from_rgb(107, 37, 249)
+mainshop = [{"name": "Item", "price": 100, "description": "remember to take this out of the bot"},
+            {"name": "Item1", "price": 100, "description": "remember to take this out of the bot"},
+            {"name": "Item2", "price": 100, "description": "remember to take this out of the bot"},
+            {"name": "Item3", "price": 100, "description": "remember to take this out of the bot"},
+            {"name": "Item4", "price": 100, "description": "remember to take this out of the bot"}]
+
 
 class economy(commands.Cog):
 
@@ -45,6 +51,100 @@ class economy(commands.Cog):
 
         bal = [users[str(user.id)]['wallet'], users[str(user.id)]['bank']]
         return bal
+
+    async def buy_this(self, user, item_name, amount):
+        item_name = item_name.lower()
+        name_ = None
+        for item in mainshop:
+            name = item["name"].lower()
+            if name == item_name:
+                name_ = name
+                price = item["price"]
+                break
+
+        if name_ == None:
+            return [False, 1]
+
+        cost = price * amount
+
+        users = await self.get_bank_data()
+
+        bal = await self.update_bank(user)
+
+        if bal[0] < cost:
+            return [False, 2]
+
+        try:
+            index = 0
+            t = None
+            for thing in users[str(user.id)]["bag"]:
+                n = thing["item"]
+                if n == item_name:
+                    old_amt = thing["amount"]
+                    new_amt = old_amt + amount
+                    users[str(user.id)]["bag"][index]["amount"] = new_amt
+                    t = 1
+                    break
+                index += 1
+            if t == None:
+                obj = {"item": item_name, "amount": amount}
+                users[str(user.id)]["bag"].append(obj)
+        except:
+            obj = {"item": item_name, "amount": amount}
+            users[str(user.id)]["bag"] = [obj]
+
+        with open("mainbank.json", "w") as f:
+            json.dump(users, f)
+
+        await self.update_bank(user, cost * -1, "wallet")
+
+        return [True, "Worked"]
+
+    async def sell_this(self, user, item_name, amount, price=None):
+        item_name = item_name.lower()
+        name_ = None
+        for item in mainshop:
+            name = item["name"].lower()
+            if name == item_name:
+                name_ = name
+                if price == None:
+                    price = 0.9 * item["price"]
+                break
+
+        if name_ == None:
+            return [False, 1]
+
+        cost = price * amount
+
+        users = await self.get_bank_data()
+
+        bal = await self.update_bank(user)
+
+        try:
+            index = 0
+            t = None
+            for thing in users[str(user.id)]["bag"]:
+                n = thing["item"]
+                if n == item_name:
+                    old_amt = thing["amount"]
+                    new_amt = old_amt - amount
+                    if new_amt < 0:
+                        return [False, 2]
+                    users[str(user.id)]["bag"][index]["amount"] = new_amt
+                    t = 1
+                    break
+                index += 1
+            if t == None:
+                return [False, 3]
+        except:
+            return [False, 3]
+
+        with open("mainbank.json", "w") as f:
+            json.dump(users, f)
+
+        await self.update_bank(user, cost, "wallet")
+
+        return [True, "Worked"]
 
     @commands.command(aliases=['bal'])
     @cooldown(1, 3, BucketType.user)
@@ -222,11 +322,6 @@ class economy(commands.Cog):
     @commands.command()
     async def shop(self, ctx):
         embed = discord.Embed(title="Shop", colour=embedColor)
-        mainshop = [{"name": "Item", "price": 100, "description": "remember to take this out of the bot"},
-                    {"name": "Item1", "price": 100, "description": "remember to take this out of the bot"},
-                    {"name": "Item2", "price": 100, "description": "remember to take this out of the bot"},
-                    {"name": "Item3", "price": 100, "description": "remember to take this out of the bot"},
-                    {"name": "Item4", "price": 100, "description": "remember to take this out of the bot"}]
 
         for item in mainshop:
             name = item["name"]
@@ -236,9 +331,64 @@ class economy(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @commands.command()
+    async def buy(self, ctx, item, amount=1):
+        await self.open_account(ctx.author)
+
+        res = await self.buy_this(ctx.author, item, amount)
+
+        if not res[0]:
+            if res[1] == 1:
+                await ctx.send("That Object isn't there!")
+                return
+            if res[1] == 2:
+                await ctx.send(f"You don't have enough money in your wallet to buy {amount} {item}")
+                return
+
+        await ctx.send(f"You just bought {amount} {item}")
+
+    @commands.command(aliases=['inventory'])
+    async def inv(self, ctx):
+        await self.open_account(ctx.author)
+        user = ctx.author
+        users = await self.get_bank_data()
+
+        try:
+            bag = users[str(user.id)]["bag"]
+        except:
+            bag = []
+
+        em = discord.Embed(title="Your Inventory")
+        for item in bag:
+            name = item["item"]
+            amount = item["amount"]
+
+            em.add_field(name=name, value=amount)
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def sell(self, ctx, item, amount=1):
+        await self.open_account(ctx.author)
+
+        res = await self.sell_this(ctx.author, item, amount)
+
+        if not res[0]:
+            if res[1] == 1:
+                await ctx.send("That Object isn't there!")
+                return
+            if res[1] == 2:
+                await ctx.send(f"You don't have {amount} {item} in your bag.")
+                return
+            if res[1] == 3:
+                await ctx.send(f"You don't have {item} in your bag.")
+                return
+
+        await ctx.send(f"You just sold {amount} {item}.")
 
 
-        
+
+
 
 def setup(client):
 	client.add_cog(economy(client)) 
