fd5a45100db31b3745abbaa75b0af1b2eba3a9d5
InsrtRandomUserHere <72641014+InsrtRandomUserHere@users.noreply.github.com>
2021-08-02 12:41:41 +0800

    refactor text commands

--- cogs/cmds_text.py
+++ cogs/cmds_text.py
@@ -9,86 +9,79 @@ class TextFilter(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-    def print_cool(self, text):
-        cool_text = Figlet()
-        return str(cool_text.renderText(text))
+    def ascii(self, text):
+        font_chosen = Figlet()
+        return str(font_chosen.renderText(text))
 
-    def print_cool1(self, text):
-        cool_text = Figlet(font='slant')
-        return str(cool_text.renderText(text))
+    def ascii_slant(self, text):
+        font_chosen = Figlet(font='slant')
+        return str(font_chosen.renderText(text))
 
-    def print_cool3(self, text):
-        cool_text = Figlet(font="3-d")
-        return str(cool_text.renderText(text))
+    def _3d(self, text):
+        font_chosen = Figlet(font="3-d")
+        return str(font_chosen.renderText(text))
 
-    def print_cool4(self, text):
-        cool_text = Figlet(font="3x5")
-        return str(cool_text.renderText(text))
+    def tag(self, text):
+        font_chosen = Figlet(font="3x5")
+        return str(font_chosen.renderText(text))
 
-    def print_cool5(self, text):
-        cool_text = Figlet(font="alligator")
-        return str(cool_text.renderText(text))
+    def fade(self, text):
+        font_chosen = Figlet(font="alligator")
+        return str(font_chosen.renderText(text))
 
-    def print_cool6(self, text):
-        cool_text = Figlet(font="dotmatrix")
-        return str(cool_text.renderText(text))
+    def dot(self, text):
+        font_chosen = Figlet(font="dotmatrix")
+        return str(font_chosen.renderText(text))
 
-    def print_cool7(self, text):
-        cool_text = Figlet(font="bubble")
-        return str(cool_text.renderText(text))
+    def bubble(self, text):
+        font_chosen = Figlet(font="bubble")
+        return str(font_chosen.renderText(text))
 
-    def print_cool8(self, text):
-        cool_text = Figlet(font="digital")
-        return str(cool_text.renderTexttext)
+    def digital(self, text):
+        font_chosen = Figlet(font="digital")
+        return str(font_chosen.renderText(text))
 
     @commands.command()
     async def slant(self, ctx, *, word):
-        fontoutput = self.print_cool1(word)
-
+        font_output = self.ascii_slant(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command(aliases=['3d'])
     async def _3d(self, ctx, *, word):
-        fontoutput = self.print_cool3(word)
-
+        font_output = self._3d(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def hashtag(self, ctx, *, word):
-        fontoutput = self.print_cool4(word)
-
+        font_output = self.tag(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def fade(self, ctx, *, word):
-        fontoutput = self.print_cool5(word)
-
+        font_output = self.fade(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def dot(self, ctx, *, word):
-        fontoutput = self.print_cool6(word[:2000])
-
+        font_output = self.dot(word[:2000])
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def bubble(self, ctx, *, word):
-        fontoutput = self.print_cool7(word)
-
+        font_output = self.bubble(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def digital(self, ctx, *, word):
-        fontoutput = self.self.print_cool8(word)
-
+        font_output = self.self.digital(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
     @commands.command()
     async def mirror(self, ctx, *, phrase):
@@ -96,17 +89,10 @@ class TextFilter(commands.Cog):
 
     @commands.command()
     async def ascii(self, ctx, *, word):
-        fontoutput = self.print_cool(word)
-
+        font_output = self.ascii(word)
         await ctx.send(
-            f'```\n{fontoutput}\n\nBy: @{ctx.message.author.name}```')
+            f'```\n{font_output}\n\nBy: @{ctx.author.name}```')
 
 
 def setup(client):
     client.add_cog(TextFilter(client))
-import discord
-from discord.ext import commands
-from pyfiglet import Figlet
-
-
-embedColor = discord.Colour.from_rgb(107, 37, 249)
